7a87cb93463e0e88e0f7e4dbb763d6eb9e0f77a4
Fabrizio <fabrynet.bianco@gmail.com>
2020-10-13 10:41:05 +0200

    Prova.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                             <h1>guido 0/</h1>
+                            <h1>Fabrizio</h1>
                         </div>
                     @endif
 
