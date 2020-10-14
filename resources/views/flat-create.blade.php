ddb554eff9edb22083f49d15fa1c870ac60d60f3
Guido <molido@hotmail.it>
2020-10-14 12:45:43 +0200

    Flat form + create + store + routes
    Vanno aggiunti l'input per l'immagine, l'input dell'indirizzo e aggiustamento grafico.
    Aggiunti gli input mancanti e testato il form vanno aggiunte le validations

---
+++ resources/views/flat-create.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.app')
+@section('content')
+
+{{-- CREAZIONE APPARTAMENTO --}}
+
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                {{-- Titolo pagina cardheader --}}
+                  <div class="card-header">
+                    <h1>New Flat:</h1>
+                  </div>
+                  <div class="card-body">
+                    {{-- FORM --}}
+                    <form action="{{route('store-flat')}}" method="post">
+                      @csrf
+                      @method('POST')
+                      {{-- Titolo --}}
+                      <div class="form-group">
+                        <label for="title">Title:</label>
+                        <input type="text" name="title" value="">
+                      </div>
+                      {{-- Descrizione --}}
+                      <div class="form-group">
+                        <label for="desc">Description:</label><br>
+                        <textarea name="desc" rows="8" cols="80">{{ old('desc') }}</textarea>
+                      </div>
+                      {{-- Indirizzo --}}
+                      <?php // DEBUG: Come trasformiamo l'indirizzo in coordinate? ?>
+                      {{-- <div class="form-group">
+                        <label for="views">Address:</label>
+                        <input type="text" name="address" value="">
+                      </div> --}}
+                      <hr>
+                      {{-- n* Stanze --}}
+                      <div class="form-group">
+                        <label for="rooms">Rooms:</label>
+                        <input type="number" name="rooms" value="{{ old('n_rooms') }}">
+                      </div>
+                      {{-- n* letti --}}
+                      <div class="form-group">
+                        <label for="beds">Beds:</label>
+                        <input type="number" name="beds" value="{{ old('n_beds') }}">
+                      </div>
+                      {{-- n* Bagni --}}
+                      <div class="form-group">
+                        <label for="bathrooms">Bathrooms:</label>
+                        <input type="number" name="bathrooms" value="{{ old('n_bathrooms') }}">
+                      </div>
+                      {{-- metri quadri --}}
+                      <div class="form-group">
+                        <label for="sqm">Size:</label>
+                        <input type="number" name="sqm" value="{{ old('sqm') }}"><span> m²</span>
+                      </div>
+                      {{-- servizi aggiuntivi --}}
+                      <div class="form-group">
+                        <label>Additional Services:</label>
+                        <ul>
+                          <li>
+                            {{-- wifi --}}
+                            <label for="wifi">Wifi:</label>
+                            <input type="checkbox" name="wifi" value="{{ old('wifi') }}">
+                          </li>
+                          <li>
+                            {{-- parking --}}
+                            <label for="parking">Parking:</label>
+                            <input type="checkbox" name="parking" value="{{ old('parking') }}">
+                          </li>
+                          <li>
+                            {{-- swim --}}
+                            <label for="swim">Swimming Pool:</label>
+                            <input type="checkbox" name="swim" value="{{ old('swim') }}">
+                          </li>
+                          <li>
+                            {{-- concierge --}}
+                            <label for="concierge">Concierge:</label>
+                            <input type="checkbox" name="concierge" value="{{ old('concierge') }}">
+                          </li>
+                          <li>
+                            {{-- sauna --}}
+                            <label for="sauna">Sauna:</label>
+                            <input type="checkbox" name="sauna" value="{{ old('sauna') }}">
+                          </li>
+                          <li>
+                            {{-- sea --}}
+                            <label for="sea">Sea View:</label>
+                            <input type="checkbox" name="sea" value="{{ old('sea') }}">
+                          </li>
+                        </ul>
+                      </div>
+
+                      <?php // DEBUG: MANCA L'INPUT D'INSERIMENTO PER L'IMMAGINE ?>
+
+                      <button class="btn btn-primary" type="submit">Confirm</button>
+                    </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +11,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// UPRA functions
 Route::get('/upra', 'LoggedController@index') -> name('upra');
+
+// Creazione flat
+Route::get('/create/flat', 'LoggedController@create') -> name('create-flat');
+Route::post('/create/flat', 'LoggedController@store') -> name('store-flat');
