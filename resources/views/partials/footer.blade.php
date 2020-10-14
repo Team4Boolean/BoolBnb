f39697c7fd164a3f543fbc53f7e82a91400ddbed
Elena <elenadangelo00@gmail.com>
2020-10-14 23:33:07 +0200

    piccoli accorgimenti footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,56 +1,62 @@
 <footer>
-  <div class="float-left col-xs-12 col-md-2 offset-md-2 col-lg-2  offset-xl-2">
-    <ul>
-      <li><h5>Informazioni</h5></li>
-      <li> <a href="#">Come funziona BoolBnb</a></li>
-      <li> <a href="#">Newsroom</a></li>
-      <li> <a href="#">BoolBnb Plus</a></li>
-      <li> <a href="#">BoolBnb Luxe</a></li>
-      <li> <a href="#">HotelTonight</a></li>
-      <li> <a href="#">Airbnb for Work</a></li>
-      <li> <a href="#">Olimpiadi</a></li>
-      <li> <a href="#">Opportunità di lavoro</a></li>
-    </ul>
+  <div class="row row-top">
+    <div class="col-xs-12 col-sm-6 col-md-3">
+      <ul>
+        <li><h5>Informazioni</h5></li>
+        <li> <a href="#">Come funziona BoolBnb</a></li>
+        <li> <a href="#">Newsroom</a></li>
+        <li> <a href="#">BoolBnb Plus</a></li>
+        <li> <a href="#">BoolBnb Luxe</a></li>
+        <li> <a href="#">HotelTonight</a></li>
+        <li> <a href="#">Airbnb for Work</a></li>
+        <li> <a href="#">Olimpiadi</a></li>
+        <li> <a href="#">Opportunità di lavoro</a></li>
+      </ul>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-3">
+      <ul>
+        <li><h5>Community</h5></li>
+        <li> <a href="#">Diversità e appartenenza</a></li>
+        <li> <a href="#">Accessibilità</a></li>
+        <li> <a href="#">Airbnb Associates</a></li>
+        <li> <a href="#">Alloggi per l'emergenza</a></li>
+        <li> <a href="#">Invita degli amici</a></li>
+      </ul>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-3">
+      <ul>
+        <li><h5>Ospita</h5></li>
+        <li> <a href="#">Diventa un host</a> </li>
+        <li> <a href="#">Offri un'Esperienza online</a> </li>
+        <li> <a href="#">Offri un'Esperienza</a> </li>
+        <li> <a href="#">Ospitare responsabilmente</a> </li>
+        <li> <a href="#">Open Homes</a> </li>
+        <li> <a href="#">Centro risorse</a> </li>
+        <li> <a href="#">Community Center</a> </li>
+      </ul>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-3">
+      <ul>
+        <li><h5>Assistenza</h5></li>
+        <li><a href="#">Aggiornamenti sulla pandemia di COVID-19</a></li>
+        <li><a href="#">Centro Assistenza</a></li>
+        <li><a href="#">Opzioni di cancellazione</a></li>
+        <li><a href="#">Servizio di supporto al vicinato</a></li>
+        <li><a href="#">Affidabilità e sicurezza</a></li>
+      </ul>
+    </div>
   </div>
 
-  <div class="float-left col-xs-12 col-md-2 col-lg-2 ">
-    <ul>
-      <li><h5>Community</h5></li>
-      <li> <a href="#">Diversità e appartenenza</a></li>
-      <li> <a href="#">Accessibilità</a></li>
-      <li> <a href="#">Airbnb Associates</a></li>
-      <li> <a href="#">Alloggi per l'emergenza</a></li>
-      <li> <a href="#">Invita degli amici</a></li>
-    </ul>
-  </div>
+  {{-- Separatore --}}
+  <div class="divider"></div>
 
-  <div class="float-left col-xs-12 col-md-2 col-lg-2 ">
-    <ul>
-      <li><h5>Ospita</h5></li>
-      <li> <a href="#">Diventa un host</a> </li>
-      <li> <a href="#">Offri un'Esperienza online</a> </li>
-      <li> <a href="#">Offri un'Esperienza</a> </li>
-      <li> <a href="#">Ospitare responsabilmente</a> </li>
-      <li> <a href="#">Open Homes</a> </li>
-      <li> <a href="#">Centro risorse</a> </li>
-      <li> <a href="#">Community Center</a> </li>
-    </ul>
-  </div>
-
-  <div class="float-left col-xs-12 col-md-2 col-lg-2 ">
-    <ul>
-      <li><h5>Assistenza</h5></li>
-      <li><a href="#">Aggiornamenti sulla pandemia di COVID-19</a></li>
-      <li><a href="#">Centro Assistenza</a></li>
-      <li><a href="#">Opzioni di cancellazione</a></li>
-      <li><a href="#">Servizio di supporto al vicinato</a></li>
-      <li><a href="#">Affidabilità e sicurezza</a></li>
-    </ul>
-  </div>
   {{-- Parte inferiore del footer --}}
-  <div class="footer-sud border-bottom">
-    <div class="float-left col-xs-12 col-md-12 col-lg-8 offset-xl-2">
-      <div class="float-left col-lg-7">
+  <div class="footer-bottom">
+    <div class="row row-bottom">
+      <div class="col-xs-12 col-lg-7">
         <ul>
           <li>© 2020 BoolBnb, Inc. All rights reserved</li>
           <li><a href="#">Privacy</a></li>
@@ -59,17 +65,17 @@
           <li><a href="#">Dettagli dell'azienda</a></li>
         </ul>
       </div>
-      <div class="float-left col-xs-12 col-md-12 col-lg-3 offset-xl-1">
+
+      <div class="col-xs-12 col-lg-5">
         <ul>
           <li><a href="#"><i class="fas fa-globe"></i> Italiano(IT) </a></li>
           <li><a href="#"> <i class="fas fa-euro-sign"> EUR</i> </a></li>
-          <li><a href="#"> <i class="fab fa-instagram"></i> </a></li>
           <li><a href="#"> <i class="fab fa-facebook-f"></i> </a></li>
           <li><a href="#"> <i class="fab fa-twitter"></i> </a></li>
+          <li><a href="#"> <i class="fab fa-instagram"></i> </a></li>
         </ul>
       </div>
     </div>
-
   </div>
 
 
