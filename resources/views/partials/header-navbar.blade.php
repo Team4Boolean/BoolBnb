57dc77dab604f6b2d78595131570395e6fe48965
Elena <elenadangelo00@gmail.com>
2020-10-14 19:43:18 +0200

    Header-navbar responsive

--- resources/views/partials/header-navbar.blade.php
+++ resources/views/partials/header-navbar.blade.php
@@ -1,10 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-white bg-white">
   <div class="row">
+
     {{-- Brand --}}
     <div class="d-md-block col-md-1 col-lg-4">
       <a id="link-logo" class="navbar-brand" href="{{ url('/') }}">
         <img id="img-logo" src="{{asset('imgs/airbnb.svg')}}" width="30px" height="auto" alt="logo">
-        <span id="span-logo">airbnb</span>
+        <span id="span-logo">boolbnb</span>
       </a>
     </div>
 
@@ -18,81 +19,66 @@
       </form>
     </div>
 
-
     <!-- Right Side Of Navbar -->
     <div class="col-xs-12 col-md-6 col-lg-4">
-      <ul class="navbar-nav">
+      <ul>
         <li class="nav-item">
           <a class="nav-link" href="#">Diventa un host</a>
         </li>
-        <!-- Authentication Links -->
-        @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-            @endif
-        @else
-          <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }}
+
+        {{-- Dropdown --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bars"></i>
+            {{-- If the user is logged shows his firstname --}}
+            @guest
+              <i class="fas fa-user"></i>
+            @else
+              <span>{{ Auth::user()->firstname }}</span>
+            @endguest
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            {{-- If the user is a guest, he can login/register --}}
+            @guest
+              <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+              <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+            {{-- If he is already logged he can logout --}}
+            @else
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                 onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
               </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+              </form>
+            @endguest
 
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                     onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
-                  </a>
+            <div class="dropdown-divider"></div>
 
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                      @csrf
-                  </form>
-              </div>
-          </li>
-        @endguest
+            {{-- Other links --}}
+            <a class="dropdown-item" href="#">Diventa un host</a>
+            <a class="dropdown-item" href="#">Proponi un'esperienza</a>
+            <a class="dropdown-item" href="#">Assistenza</a>
+          </div>
+        </li>
       </ul>
     </div>
-
   </div>
-
-{{--
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button> --}}
-
-  {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div> --}}
-
-
-
 </nav>
+
+{{-- Navbar bottom page --}}
+<div id="bottom-nav" class="navbar navbar-white bg-white">
+  <div class="login">
+    <a href="{{ route('login') }}">
+      <i class="fas fa-sign-in-alt"></i>
+      Login
+    </a>
+  </div>
+  <div class="register">
+    <a href="{{ route('register') }}">
+      <i class="fas fa-plus"></i>
+      Register
+    </a>
+  </div>
+</div>
