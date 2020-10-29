0d359f88150a50b286e3c6723cb0515354f2a870
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-10-29 12:03:50 +0100

    ADD JS MAIL

--- resources/views/flats/messages.blade.php
+++ resources/views/flats/messages.blade.php
@@ -10,9 +10,9 @@
          <ul>
             @foreach ($messages as $message)
               <li>
-                <div class="user" id="{{$message -> id}}" >
-                  <h6><strong>From: </strong> {{$message -> email}}</h5>
-                  <p><strong>Testo messaggio: </strong>{{$message -> message}}</p>
+                <div class="mail" created="{{$message -> created_at}}" >
+                  <h6><strong>From: </strong> <span class="sender">{{$message -> email}}</span></h6>
+                  <p><strong>Testo messaggio: </strong><span id="message">{{$message -> message}}</span></p>
                 </div>
               </li>
 
@@ -26,9 +26,64 @@
          <h3>Testo della Mail</h3>
        </div>
        <div class="row row-text border">
+         <div class="">
+           <h3>Messaggio da: </h3>
+           <span id="show-sender"></span>
+         </div>
+         <br>
+         <div class="">
+           <h4>Inviato alle: </h4>
+           <span id="show-date"></span>
+         </div>
+         <p id="show-message"></p>
        </div>
      </div>
    </div>
   </div>
+
+  <script type="text/javascript">
+
+    function clickMail() {
+
+      $('.mail').click(function(){
+        $('.mail').removeClass('active');
+        $(this).addClass('active');
+
+      showMailData();
+      })
+    }
+
+    function showMailData() {
+
+      var sender = $('.active').find('.sender');
+      var senderText = sender.html();
+      var target =  $('#show-sender');
+      target.html('');
+      target.append(senderText);
+
+      var date = $('.active').attr('created');
+      var target =  $('#show-date');
+      target.html('');
+      target.append(date);
+
+      var message = $('.active').find('#message');
+      var messageText = message.html();
+      var target =  $('#show-message');
+      target.html('');
+      target.append(messageText);
+
+    }
+
+    function init(){
+
+      var target = $('.mail').first();
+      target.addClass('active');
+      showMailData();
+
+      clickMail();
+    }
+
+    $(document).ready(init);
+  </script>
 
 @endsection
