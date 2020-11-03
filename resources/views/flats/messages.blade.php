ff73a630e68999c09526f51c4d16bc7b1fbf6ba6
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-11-03 17:24:42 +0100

    card mail

--- resources/views/flats/messages.blade.php
+++ resources/views/flats/messages.blade.php
@@ -1,45 +1,65 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container-mail container">
-    <div class="row row-mail">
-      <div class="mail-col col-xs-12 col-md-4 col-xl-3">
-       <div class="row title-mail border">
-         <a  href="{{ url()->previous() }}"> <i class="fas fa-arrow-circle-left"></i> </a>
-         <h3>MAIL RICEVUTE</h3>
-       </div>
-       <div class="row contacts ">
-         <ul id="mail-list">
-            @foreach ($messages as $message)
-              <li>
-                <div class="mail" created="{{$message -> created_at}}" >
-                  <h6><strong>Inviato da: </strong> <span class="sender">{{$message -> email}}</span></h6>
 
-                  <p class="clamp-line"><strong>Testo messaggio: </strong><span class="message">{{$message -> message}}</span></p>
+
+
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <div class="card">
+          <div class="card-header ">
+            <a style="font-size: 40px" href="{{ url()->previous() }}"><i class="fas fa-arrow-circle-left"></i></a>
+            <h1 >CASELLA MAIL</h1>
+          </div>
+          <div class="card-body">
+            <div class="container-mail container">
+              <div class="row row-mail">
+                <div class="mail-col  col-lg-4 ">
+                  <div class="row title-mail border">
+
+                    <h3>MAIL RICEVUTE</h3>
+                  </div>
+                  <div class="row contacts ">
+                    <ul id="mail-list">
+                      @foreach ($messages as $message)
+                        <li>
+                          <div class="mail" created="{{$message -> created_at}}" >
+                            <h6><strong>Inviato da: </strong> <span class="sender">{{$message -> email}}</span></h6>
+
+                            <p class="clamp-line"><strong>Testo messaggio: </strong><span class="message">{{$message -> message}}</span></p>
+                          </div>
+                        </li>
+
+                      @endforeach
+
+                    </ul>
+                  </div>
                 </div>
-              </li>
+                <div class="mail-right-side mail-col  col-lg-8 ">
+                  <div class="row row-messages border">
+                    <h3><T>TESTO DELLA MAIL</T></h3>
+                  </div>
+                  <div class="row row-text border" id="mail-body">
+                    <div class="messages-received ">
+                      <h6>Messaggio da: </h6>
+                      <span id="show-sender"></span>
+                      <h6>Inviato alle:</h6>
+                      <span id="show-date"></span>
+                    </div>
+                    <br>
+                    <p id="show-message"></p>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-            @endforeach
-
-         </ul>
-       </div>
-     </div>
-     <div class="mail-right-side mail-col col-xs-12 col-md-8 col-xl-9">
-       <div class="row row-messages border">
-         <h3><T>TESTO DELLA MAIL</T></h3>
-       </div>
-       <div class="row row-text border" id="mail-body">
-         <div class="messages-received ">
-           <h6>Messaggio da: </h6>
-           <span id="show-sender"></span>
-           <h6>Inviato alle:</h6>
-           <span id="show-date"></span>
-         </div>
-         <br>
-         <p id="show-message"></p>
-       </div>
-     </div>
-   </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
 
   <script type="text/javascript">
 
